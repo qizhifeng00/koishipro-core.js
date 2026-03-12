fe16d976d47e701307fdd27889bc20aeab6d45c9
purerosefallen <78877@qq.com>
2026-03-12 11:40:57 +0800

    add resolveRuleCode for createReader

--- src/card-reader/sqljs-card-reader.ts
+++ src/card-reader/sqljs-card-reader.ts
@@ -1,27 +1,50 @@
 import type { Database, SqlJsStatic } from 'sql.js';
 import { YGOProCdb } from 'ygopro-cdb-encode';
+import { OcgcoreCommonConstants } from 'ygopro-msg-encode';
 import type { CardReaderFinalized, CardReaderFn } from '../types/callback';
 
 function createReader(dbs: YGOProCdb[]): CardReaderFn {
   const brokenDbIndexes = new Set<number>();
   return (cardId: number) => {
+    let data: ReturnType<YGOProCdb['findById']>;
     for (let i = 0; i < dbs.length; i++) {
       if (brokenDbIndexes.has(i)) {
         continue;
       }
-      const db = dbs[i];
-      let data;
       try {
-        data = db.findById(cardId);
+        data = dbs[i].findById(cardId);
       } catch {
         brokenDbIndexes.add(i);
         continue;
       }
       if (data) {
-        return data;
+        break;
       }
     }
-    return null;
+    if (!data) {
+      return null;
+    }
+    if (
+      data.alias &&
+      !data.ruleCode &&
+      !(data.type & OcgcoreCommonConstants.TYPE_TOKEN)
+    ) {
+      for (let i = 0; i < dbs.length; i++) {
+        if (brokenDbIndexes.has(i)) {
+          continue;
+        }
+        try {
+          dbs[i].resolveRuleCode([data]);
+        } catch {
+          brokenDbIndexes.add(i);
+          continue;
+        }
+        if (data.ruleCode) {
+          break;
+        }
+      }
+    }
+    return data;
   };
 }
 
