4778ac35c00795d9ff6ea8a937ba790362ce9a94
purerosefallen <78877@qq.com>
2026-03-20 11:50:18 +0800

    Align query buffer with gframe server mode

--- src/constants/ocgcore.ts
+++ src/constants/ocgcore.ts
@@ -1,7 +1,7 @@
 import { OcgcoreCommonConstants } from '../vendor';
 
 export const MESSAGE_BUFFER_SIZE = OcgcoreCommonConstants.SIZE_MESSAGE_BUFFER;
-export const QUERY_BUFFER_SIZE = 0x4000;
+export const QUERY_BUFFER_SIZE = 0x40000;
 export const REGISTRY_BUFFER_SIZE = 0x2000;
 
 export const LEN_FAIL = 0;
